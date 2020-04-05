9c1cf03406c18b92054005ac89fb66c2a7289c70
Michael <mishaba1990@gmail.com>
2020-04-05 21:22:14 +0300

    added color to table values

--- static/src/app.js
+++ static/src/app.js
@@ -36,8 +36,46 @@ $(document).ready(() =>{
         let dataRow = resp[i];
         let htmlRow = $("<tr />");
         for (let td = 0; td < dataRow.length; td++) {
+            var trCellDataClass = "";
+            if (td===6){
+                var trCellData=parseFloat(dataRow[td]);
+              // switch(trCellData){
+              //     // case trCellData>5.0:
+              //     // trCellDataClass="bigPlus";
+              //     // break;
+              //     case trCellData>0&&trCellData<=5.0:
+              //     trCellDataClass="smallPlus";
+              //     break;
+              //     case trCellData<0&&trCellData>-5.0:
+              //     trCellDataClass="smallMinus";
+              //     break;
+              //     case trCellData<=-5.0:
+              //     trCellDataClass="smallPlus";
+              //     break;
+              //     default:
+              //     trCellDataClass="zeroValue";
+              //     break;
+            
+              if ( trCellData>5) {
+                trCellDataClass="bigPlus";
+              } else if (trCellData>0&&trCellData<=5.0) {
+                trCellDataClass="smallPlus";
+              }else if (trCellData<0&&trCellData>-5.0) {
+                trCellDataClass="smallMinus";
+              }else if (trCellData<=-5.0) {
+                trCellDataClass="bigMinus";
+              }else{
+                trCellDataClass="zeroValue";
+              }
+            
+              $(htmlRow).append("<td class="+ trCellDataClass +">" + dataRow[td] + "</td>");
+            } 
+            else {
+              $(htmlRow).append("<td >" + dataRow[td] + "</td>");
+            }
+            // if trCellData.length
 
-            $(htmlRow).append("<td>" + dataRow[td] + "</td>");
+          
         }
         $("#charts tbody").append(htmlRow);
     }
@@ -52,10 +90,10 @@ var navbar = new Vue({
     el : "nav",
     data: function () {
         return {
-            title: "Django Stock",
+            title: "S&P 500 Market",
             buttons : [
                 {
-                    text:"Home", 
+                    text:"Real Estate", 
                     href: "#home"
                 },
                 {
