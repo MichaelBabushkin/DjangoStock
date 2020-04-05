147eb3698530c7e0f2a16f71f290dcb36091c9a2
weirdox <marie.berezhna@gmail.com>
2020-04-06 00:57:29 +0300

    charts vue instance

--- static/src/app.js
+++ static/src/app.js
@@ -1,72 +1,80 @@
-
-$(document).ready(() =>{
-    $.ajax( "/stats/" )
-  .done(function(resp) {
-    resp = $.parseJSON(resp);
-    let table = `<table>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Symbol</th>
-        <th>Stock Market</th>
-        <th>Last Deal</th>
-        <th>Last Rate</th>  
-        <th>Daily Change</th>
-        <th>Daily Change in %</th>
-        <th>Trade Volume</th>
-        <th>Daily Max</th>
-        <th>Daily Min</th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-    <tfoot>
-      <tr>
-        <td>Sum</td>
-        <td>$180</td>
-      </tr>
-    </tfoot>
-  </table>`;
-
-
-    $("#charts").empty().append(table);
-
-
-    for (let i = 0; i < resp.length; i++) {
-        let dataRow = resp[i];
-        let htmlRow = $("<tr />");
-        for (let td = 0; td < dataRow.length; td++) {
-            var trCellDataClass = "";
-            if (td===6){
-              var trCellData=parseFloat(dataRow[td]);
-              if ( trCellData>5) {
-                trCellDataClass="bigPlus";
-              } else if (trCellData>0&&trCellData<=5.0) {
-                trCellDataClass="smallPlus";
-              }else if (trCellData<0&&trCellData>-5.0) {
-                trCellDataClass="smallMinus";
-              }else if (trCellData<=-5.0) {
-                trCellDataClass="bigMinus";
-              }else{
-                trCellDataClass="zeroValue";
-              }
-              $(htmlRow).append("<td class="+ trCellDataClass +">" + dataRow[td] + "</td>");
-            } 
-            else {
-              $(htmlRow).append("<td >" + dataRow[td] + "</td>");
+Vue.config.devtools = true;
+var charts = new Vue ({
+ 
+  delimiters: ['[[', ']]'],
+  el : "#charts",
+  data: {
+    template : `<table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Symbol</th>
+          <th>Stock Market</th>
+          <th>Last Deal</th>
+          <th>Last Rate</th>  
+          <th>Daily Change</th>
+          <th>Daily Change in %</th>
+          <th>Trade Volume</th>
+          <th>Daily Max</th>
+          <th>Daily Min</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Sum</td>
+          <td>$180</td>
+        </tr>
+      </tfoot>
+    </table>`
+  },
+  methods: {
+    updateTable () {
+      let object = this;
+      $.ajax( "/stats/" )
+      .done(function(resp) {
+        resp = $.parseJSON(resp);
+        $("#charts").empty().append(object.template);
+        for (let i = 0; i < resp.length; i++) {
+            let dataRow = resp[i];
+            let htmlRow = $("<tr />");
+            for (let td = 0; td < dataRow.length; td++) {
+                var trCellDataClass = "";
+                if (td===6){
+                  var trCellData=parseFloat(dataRow[td]);
+                  if ( trCellData>5) {
+                    trCellDataClass="bigPlus";
+                  } else if (trCellData>0&&trCellData<=5.0) {
+                    trCellDataClass="smallPlus";
+                  }else if (trCellData<0&&trCellData>-5.0) {
+                    trCellDataClass="smallMinus";
+                  }else if (trCellData<=-5.0) {
+                    trCellDataClass="bigMinus";
+                  }else{
+                    trCellDataClass="zeroValue";
+                  }
+                  $(htmlRow).append("<td class="+ trCellDataClass +">" + dataRow[td] + "</td>");
+                } 
+                else {
+                  $(htmlRow).append("<td >" + dataRow[td] + "</td>");
+                }
             }
-            // if trCellData.length
-
-          
+            $("#charts tbody").append(htmlRow);
         }
-        $("#charts tbody").append(htmlRow);
+      })
+      .fail(function() {
+        alert( "error" );
+      });
     }
-    
-  })
-  .fail(function() {
-    alert( "error" );
-  });
+  },
+  created () {
+    this.updateTable();
+  },
+  computed: {
+  }
 });
+
 var navbar = new Vue({
     delimiters: ['[[', ']]'],
     el : "nav",
@@ -88,13 +96,13 @@ var navbar = new Vue({
     beforeCreate () {
         Vue.config.devtools = true;
     },
-    created() {
+    created () {
 //
       },
-      methods: {
+    methods: {
 //
       },
-      computed: {
+    computed: {
         updateMenu() {
           return this.buttons;
         }
